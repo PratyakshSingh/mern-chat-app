2c7e4ae0f6d66943003ffbc28103df929ff3b521
PratyakshSingh <pratyaksh.singh8171@gmail.com>
2023-04-30 03:34:38 +0530

    status updated

--- server.js
+++ server.js
@@ -25,17 +25,17 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname1, "./client/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "../frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname1, "./client", "dist", "index.html"))
+);
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 //---------------deployment---------------------
 
