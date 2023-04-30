6ea3a7880e54742510aa5720d0cb6f03a49aa9db
PratyakshSingh <pratyaksh.singh8171@gmail.com>
2023-04-30 06:22:27 +0530

    updated origin for socket.io

--- server.js
+++ server.js
@@ -25,17 +25,17 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname1, "./client/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/dist")));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname1, "./client", "dist", "index.html"))
-);
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "../frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 //---------------deployment---------------------
 
@@ -51,7 +51,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*:*",
   },
 });
 
